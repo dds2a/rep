506d81fd787c1cd66e55990302a4b9ac7c778a5a
dds2a <dds2a@ya.ru>
2019-12-08 10:10:32 +1000

    смс

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,11 @@
-print('Hello World')
+from tkinter import *
+
+root = Tk()
+root.geometry('+10+10')
+root.title('Вход на GitHub')
+root.attributes('-topmost', True)
+
+canvas = Canvas(root, bg='white')
+canvas.pack()
+
+root.mainloop()
